4edd788104a08ec74be18f24618adfa6238bffe7
Arivan Ismail <187494087+arivanismail-05@users.noreply.github.com>
2025-07-15 15:19:13 +0300

    Vite : Get your build process in order with laracast 30-days or laravel

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
+      @vite(['resources/js/app.js'])
 </head>
-<body  class="h-full">
+<body   class=" body h-full">
 
 <div class="min-h-full">
   <nav class="bg-gray-800">
